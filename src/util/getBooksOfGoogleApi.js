b6e0df94e756010aaaec55775ffedc201a8063ff
felipeceballos12 <ceballos-65@hotmail.com>
2024-03-08 17:25:24 -0500

    commit 2

--- src/util/getBooksOfGoogleApi.js
+++ src/util/getBooksOfGoogleApi.js
@@ -1,16 +1,11 @@
 export default async function getBooksOfGoogleApi(name) {
-  console.log(import.meta.env.VITE_API_KEY);
   try {
-    const url =
-      'https://www.googleapis.com/books/v1/volumes?q=' +
-      name +
-      '&maxResults=40&key=' +
-      import.meta.env.VITE_API_KEY;
+    const url = 'http://localhost:3000/books/' + name;
 
     const booksResponse = await fetch(url);
-    const booksData = await booksResponse.json();
+    const book = await booksResponse.json();
 
-    return booksData.items;
+    return book.items;
   } catch (err) {
     console.log(err);
   }
